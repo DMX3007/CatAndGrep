535893ae9c73f9f72c7058332782f7c8eb9e602b
Enola Arielle <earielle@ob-d7.nsk.21-school.ru>
2021-11-23 21:02:25 +0700

    earielle add cat flags make and header

---
+++ src/cat/s21_cat.h
@@ -0,0 +1,14 @@
+#ifndef _SRC_CAT_S21_CAT_H_INCLUDED
+#define _SRC_CAT_S21_CAT_H_INCLUDED
+
+struct flags {
+    int blank;
+    int enter;
+    int number;
+    int squeeze;
+    int tabs;
+};
+
+char is_it_flag(char* string_argv_num);
+
+#endif  //  _SRC_CAT_S21_CAT_H_INCLUDED
