d0f499083716c23b5baec37f4baa8d472c1dc7af
Enola Arielle <earielle@ob-d7.nsk.21-school.ru>
2021-12-05 19:51:43 +0700

    header

--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -1,15 +1,38 @@
-#ifndef _SRC_CAT_S21_CAT_H_INCLUDED
-#define _SRC_CAT_S21_CAT_H_INCLUDED
+#ifndef SRC_CAT_S21_CAT_H_
+#define SRC_CAT_S21_CAT_H_
 
-struct flags {
-    int blank;
-    int enter;
-    int number;
-    int squeeze;
-    int tabs;
+#include <getopt.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+#define OFFSET_ASCII 64
+#define YES 1
+#define NO 0
+
+static struct option long_option[] = {{"number-nonblank", 0, 0, 'b'},
+                                      {"number", 0, 0, 'n'},
+                                      {"squeeze-blank", 0, 0, 's'},
+                                      {0, 0, 0, 0}};
+
+struct fields {
+    int b;
+    int e;
+    int n;
+    int s;
+    int t;
+    int v;
 };
 
-char is_it_flag(char** string_argv_num, char *array_of_flags);
-int is_it_file(char** string_argv_num);
+void init_struct(struct fields *flags);
+void take_flag(char option, struct fields *flags);
 
-#endif  //  _SRC_CAT_S21_CAT_H_INCLUDED
+void s_flag(char ch, char previous, int *raise, int *PRINT);
+void n_flag(int *common_count, char previous);
+void t_flag(char ch, int *PRINT);
+void b_flag(char ch, char previous, int *common_count);
+void e_flag(char ch);
+void v_flag(char ch, int *PRINT);
+void output(char ch);
+
+#endif  //  SRC_CAT_S21_CAT_H_
