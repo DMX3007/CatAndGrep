ecf979494e7d7c11a595cae21e7c73ef03ea1447
Enola Arielle <earielle@ob-d7.nsk.21-school.ru>
2021-12-09 22:10:41 +0700

    grep

--- src/grep/grep.c
+++ src/grep/grep.c
@@ -1,5 +1,9 @@
 #include <getopt.h>
+#include <pcre.h>
 #include <stdio.h>
+
+#define YES 1
+#define NO 0
 
 struct fields {
     int e;
@@ -12,21 +16,33 @@ struct fields {
     int s;
     int f;
     int o;
-    int sum;
 };
 
 void init_struct(struct fields *flags);
 void take_flag(char option, struct fields *flags);
+void re_demption(char *pattern, char *str, int *r_value);
 
 int main(int argc, char *argv[]) {
-    /*first of all we should take the flags and file from stdin then we should
-    analyze our flags and checked our file third we should format our text with
-    the parametrs of flags */
-    int option = 0;
+    char pattern[4096] = {'\0'}, *str = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    int option = 0, flag = 0;
     struct fields flags;
-    while (((option = getopt(argc, argv, "eivclnhfso")) != -1)) {
-        printf("%d", option);
+    FILE *file;
+    while (((option = getopt_long(argc, argv, "e:i:v:c:l:n:h:f:s:o:", 0, 0)) !=
+            -1)) {
         take_flag(option, &flags);
+    }
+    for (int i = 0; argv++ && (i < argc - 1); i++) {
+        if ((strlen((*argv)) > 2) && (**(argv) != '-')) {
+            if ((file = fopen(*argv, "r")) == NULL) {
+                continue;
+            } else {
+                while ((getline(&str, len, file)) != -1) {
+                    re_demption(pattern, str, &flag);
+                }
+            }
+        }
     }
     return 0;
 }
@@ -43,36 +59,55 @@ void init_struct(struct fields *flags) {
     flags->s = 0;
     flags->f = 0;
     flags->o = 0;
-    flags->sum = 0;
 }
 
 void take_flag(char option, struct fields *flags) {
     if (option == 'v') {
         flags->v = option;
-        flags->sum += 1;
     } else if (option == 'e') {
         flags->e = option;
-        flags->sum += 1;
     } else if (option == 'n') {
         flags->n = option;
-        flags->sum += 1;
     } else if (option == 's') {
         flags->s = option;
-        flags->sum += 1;
     } else if (option == 'i') {
         flags->i = option;
-        flags->sum += 1;
     } else if (option == 'c') {
         flags->c = option;
-        flags->sum += 1;
     } else if (option == 'h') {
         flags->h = option;
-        flags->sum += 1;
     } else if (option == 'f') {
         flags->f = option;
-        flags->sum += 1;
     } else if (option == 'o') {
         flags->o = option;
-        flags->sum += 1;
+    } else if (option == '?') {
+        ;
+    }
+}
+
+void re_demption(char *pattern, char *str, int *r_value) {
+    pcre *re;
+    int options = 0, erroffset;
+    const char *error;
+    re = pcre_compile((char *)pattern, options, &error, &erroffset, NULL);
+    if (!re) {
+        *r_value = NO;
+    } else {
+        int count = 0, ovector[19683];
+        count = pcre_exec(re, NULL, (char *)str, 31, 0, 0, ovector, 19683);
+        if (count < 0) {
+            printf("No match\n");
+            *r_value = NO;
+        } else {
+            for (int c = 0; c < 2 * count; c += 2) {
+                if (ovector[c] < 0) {
+                    printf("<unset>\n");
+                    *r_value = NO;
+                } else {
+                    printf("%d\n", ovector[c]);
+                    *r_value = YES;
+                }
+            }
+        }
     }
 }
